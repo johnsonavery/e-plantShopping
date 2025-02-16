be0cf26a40eadc28e0aabcc9c437ae39ca6e5da0
johnsonavery <68486281+johnsonavery@users.noreply.github.com>
2025-02-15 23:31:46 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "./",
+  build: {
+    outDir: 'dist',      // gets deployed to github pages
+    assetsDir: 'assets', // static assets (images, js, css, etc.)
+  },
   plugins: [react()],
 })
